3f2987b665ee8098ddc32c12cec4f6303ab9229f
Effiba <effiba@aims.edu.gh>
2022-09-11 14:44:33 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @void: emtpy
+ *
+ * Description: Print the size of various types of vars
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(i));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(li));
+       	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+ 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+}
