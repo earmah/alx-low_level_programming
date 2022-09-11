e7e7f93de59f3247b2b1a85b06395f6039f64657
Effiba <effiba@aims.edu.gh>
2022-09-11 14:28:28 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @void: empty
+ *
+ * Description: Print a function using printf and add warning
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
