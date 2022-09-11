f1645702dfcb69fd9bf5b369acae53b12255189d
Effiba <effiba@aims.edu.gh>
2022-09-11 14:49:24 -0700

    task 6: add return, fixed tab lacking

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu bytes(s)\n", sizeof(i));
 	printf("Size of a long int: %lu bytes(s)\n", sizeof(li));
-       	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
  	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	return 0;
 }
