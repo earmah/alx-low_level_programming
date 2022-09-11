400416d0212acadba64962b97415ace48b7b4626
Effiba <effiba@aims.edu.gh>
2022-09-11 16:39:50 -0700

    t6: rm extra s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ char c;
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu bytes(s)\n", sizeof(i));
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(li));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
