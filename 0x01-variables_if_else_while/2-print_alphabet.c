6d7edaaf99b1325fd638af6ceed18878dc271d8d
Effiba <effiba@aims.edu.gh>
2022-09-12 07:13:45 -0700

    t2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void: empty
+ *
+ * Description: print alphabet using putchar
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char alpha = 'abcdefghijklmnopqrstuvwxyz';
+
+	putchar(alpha);
+}
