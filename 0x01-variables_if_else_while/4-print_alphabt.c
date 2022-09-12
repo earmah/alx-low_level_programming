70b70cc32305badd1e15bb1418ab18c550cf76c4
Effiba <effiba@aims.edu.gh>
2022-09-12 08:18:01 -0700

    t4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void: empty
+ *
+ * Description - print alpha in lower w/o qe
+ * Return: 0
+ */
+int main(void)
+{
+	int ch_val;
+	int nl = '\n';
+
+	while (ch_val < 123)
+	{
+		if (ch_val == 101 || ch_val == 113)
+		{
+			ch_val++;
+		}
+		else
+		{
+			putchar(ch_val);
+			ch_val++;
+		}
+	}
+	return (0);
+}
