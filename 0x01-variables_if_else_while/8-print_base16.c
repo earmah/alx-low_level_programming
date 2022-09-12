8b30419abb914ccd098f41a97f45e03e9e50f464
Effiba <effiba@aims.edu.gh>
2022-09-12 09:26:58 -0700

    t8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void: empty
+ *
+ * Description - print all hexi vals in lwer case
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch1_val = 48;
+	int ch2_val = 97;
+
+	while (ch1_val < 58)
+	{
+		putchar(ch1_val);
+		ch1_val++;
+	}
+	while (ch2_val < 103)
+	{
+		putchr(ch2_val);
+		ch2_val++;
+	}
+	putchar('\n');
+	return (0);
+}
