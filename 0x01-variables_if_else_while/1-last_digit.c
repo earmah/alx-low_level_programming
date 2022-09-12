786aed73f43db24e307106b1387321e976d2e0b9
Effiba <effiba@aims.edu.gh>
2022-09-12 06:59:09 -0700

    t1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * @void: empty
+ *
+ * Description - confirm if last digit is less,
+ * more or equal to 5
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n;
+	int last_dig;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last_dig = n % 10;
+
+	if (last_dig < 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
+	}
+	else
+	if (last_dig == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_dig);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_dig);
+	}
+	return (0);
+}
