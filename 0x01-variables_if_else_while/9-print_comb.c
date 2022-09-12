d4cc38f1d8a9382ee3401ce4d87c077e9f608ba3
Effiba <effiba@aims.edu.gh>
2022-09-12 10:29:49 -0700

    t9:var switching

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,22 @@
 int main(void)
 {
 	int val = 48;
+	int com = 44;
+	int sp = 32;
+	int count = 0;
+	int temp = 0;
 
 	putchar(val);
 	val++;
 	while (val < 58)
 	{
-		putchar(44,32);
+		temp = com;
+		while (count < 2)
+		{
+			putchar(temp);
+			temp = sp;
+			count++;
+		}
 		putchar(val);
 		val++;
 	}
