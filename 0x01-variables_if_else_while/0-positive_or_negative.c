2fe92d34bccc9bb4321880521db52828e962887e
Effiba <effiba@aims.edu.gh>
2022-09-12 06:00:38 -0700

    t0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * @void: empty
+ *
+ * Description - C file that determines if input is neg, pos or zero
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("is positive\n");
+	} else if (n == 0)
+	{
+	       printf("is zero\n");
+	} else
+	{
+		printf("is negative\n");
+	}
+	return (0);
+}
