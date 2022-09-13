909bc4b3bd225dbd4cbeb65cc2b05995b8358c93
Effiba <effiba@aims.edu.gh>
2022-09-12 21:45:54 -0700

    t8: betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,11 @@
  * @void: empty
  *
  * Desciption - fcn write to print stdout
- * Return: 0
+ * Return: 1
  */
 
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	return (1);
 }
