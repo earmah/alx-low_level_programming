1b69b67fe4b564c2e53e3de1b1b0a93758850bc5
Effiba <effiba@aims.edu.gh>
2022-09-13 15:22:01 -0700

    t0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @void: empty
+ *
+ * Description - print _putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str = '_putchar';
+
+	putchar(str);
+	return (0);
+}
