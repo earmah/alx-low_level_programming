aa63bfeb5dd83023199890b9b12b5077463f0217
Effiba <effiba@aims.edu.gh>
2022-09-12 21:43:42 -0700

    adv t

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * @void: empty
+ *
+ * Desciption - fcn write to print stdout
+ * Return: 0
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
+}
