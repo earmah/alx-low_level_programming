facc35ec7ffe9a7ac82a58e1c1b688c4761b1e60
Effiba <effiba@aims.edu.gh>
2022-09-13 16:41:23 -0700

    t1: call a void function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,16 +9,7 @@
 
 int main(void)
 {
-	print_alphabet();
-	putchar(95);
-	putchar(112);
-	putchar(117);
-	putchar(116);
-	putchar(99);
-	putchar(104);
-	putchar(97);
-	putchar(114);
-	putchar('\n');
+	void print_alphabet(void);
 	return (0);
 }
 
@@ -33,4 +24,17 @@ void print_alphabet(void)
 	}
 	putchar('\n');
 	return ;
+}
+
+void _putchar(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
 }
