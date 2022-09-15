c92640b7fc3c5b9a57d9b0b332a6e09c8afcc68e
Effiba <effiba@aims.edu.gh>
2022-09-14 21:08:28 -0700

    t6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes abs val
+ * @n: input
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	else
+	{
+		return (n);
+	}
+	return (n);
+}
