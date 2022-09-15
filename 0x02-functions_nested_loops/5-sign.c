067842a9ed980863fa487933f8e4f81b21b77278
Effiba <effiba@aims.edu.gh>
2022-09-14 20:59:24 -0700

    t5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of input
+ * @n: inputted int
+ * Return: 1 if pos, 0 if 0, -1 if neg
+ */
+
+int print_sign(int n)
+{
+	int pos = 43;
+	int neg = 45;
+	int zer = 48;
+
+	if (n == 48)
+	{
+		_putchar(zer);
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar(pos);
+		return (1);
+	}
+	else
+	{
+		_putchar(neg);
+		return (-1);
+	}
+}
