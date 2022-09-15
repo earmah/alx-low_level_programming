bb13c32f02d50c182af1c66d43f03503186ebdc9
Effiba <effiba@aims.edu.gh>
2022-09-14 20:07:00 -0700

    t2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10: print the alphabetx10
+ * @void: empty
+ *
+ * Description - print alphabet x10
+ */
+
+void print_alphabet_x10(void)
+{
+	int repetition = 1;
+	int count_alpha = 97;
+
+	while (repetition < 11)
+	{
+		while (count_alpha < 123)
+		{
+			_putchar(count_alpha);
+			count_alpha++;
+		}
+		repetition++;
+	}
