64c3e7a69bafa6c33abc05e35551f7c9768a1ac9
Effiba <effiba@aims.edu.gh>
2022-09-14 21:16:54 -0700

    t7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last dig
+ * @c: input val
+ * Return: last dig
+ */
+
+int print_last_digit(int c)
+{
+	int last;
+
+	last = c % 10;
+	return (last);
+}
