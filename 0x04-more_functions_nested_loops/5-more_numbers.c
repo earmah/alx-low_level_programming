e89d264ccceb509ab42d4821392559e5d34e1ffb
Effiba <effiba@aims.edu.gh>
2022-09-15 12:02:25 -0700

    t5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints number 10 times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int level;
+	int curr_val;
+
+	for (level = 0; level < 11; ++level)
+	{
+		for (curr_val = 0; curr_val < 15; ++curr_val)
+		{
+			_putchar(curr_val + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
