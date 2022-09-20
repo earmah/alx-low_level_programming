d22ee26813dd4c6388c3ab4d46c537c38d0f6d88
Effiba <effiba@aims.edu.gh>
2022-09-19 19:56:44 -0700

    t6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2 - print every other char
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int count;
+	
+	for (; str[count] != '\0'; count++)
+	{
+		if ((count % 2) == 0)
+		{
+			_putchar(str[count]);
+		}
+		else
+		{
+			continue;
+		}
+		_putchar('\n');
+	}
+}
