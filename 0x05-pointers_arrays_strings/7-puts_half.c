50273b469b507b62346e0f537cb372e2c9f796fc
Effiba <effiba@aims.edu.gh>
2022-09-19 20:23:21 -0700

    t7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - print 2nd half of str
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int count = 0;
+
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+
+	count = (count - 1) / 2;
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+	_putchar('\n');
+}
