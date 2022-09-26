c4907c84a9ee20a8658e8ed28905a017b48dbf8f
Effiba <effiba@aims.edu.gh>
2022-09-26 16:38:41 -0700

    t0: return s instead of *s

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -11,4 +11,36 @@
 
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int i
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+/**
+ * main - check the code
+ * Return: 0
+ */
+int main(void)
+{
+	char buffer[98] = {0x00};
+
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("---------------------------\n");
+	simple_print_buffer(buffer, 98);
+	return (0);
+}
