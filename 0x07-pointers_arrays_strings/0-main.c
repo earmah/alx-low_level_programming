90eead1d454ad9a365d79e0286968d87539b544f
Effiba <effiba@aims.edu.gh>
2022-09-26 16:31:22 -0700

    t0:changed count to unsigned

---
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: nothing
+ */
+
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i
