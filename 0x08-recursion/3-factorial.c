be78fa40c8edc056792d442f0558a36b25e7cc8d
Effiba <effiba@aims.edu.gh>
2022-09-30 02:48:15 -0700

    t3: missing negative case

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
  * factorial - calculates the factorial of int
- * @n: int param 
- * 
+ * @n: int param
  * Return: int factorial
  */
 
-int factorial (int n)
+int factorial(int n)
 {
 	if (n > 1)
 	{
 		return (n * factorial(n - 1));
+	}
+	else if (n < 0)
+	{
+		return (-1);
 	}
 	else
 	{
