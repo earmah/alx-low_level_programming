730a09de0f0a2c5b1552024d16b952a27c08d3f1
Effiba <effiba@aims.edu.gh>
2022-09-30 02:19:00 -0700

    t2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints the length of a str
+ * @s: str to be counted
+ * Return: len of str
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 1;
+
+	if (*s != '\0')
+	{
+		count += _strlen_recursion(s + 1);
+	}
+	return (count);
+}
