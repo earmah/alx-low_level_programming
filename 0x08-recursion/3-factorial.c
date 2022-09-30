10d2bf6810c3d3b13c8ce7713d95958ba06adcc7
Effiba <effiba@aims.edu.gh>
2022-09-30 02:42:25 -0700

    t3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * factorial - calculates the factorial of int
+ * @n: int param 
+ * 
+ * Return: int factorial
+ */
+
+int factorial (int n)
+{
+	if (n > 1)
+	{
+		return (n * factorial(n - 1));
+	}
+	else
+	{
+		return (1);
+	}
+}
