35847088bc27377e9507a1060b83e1cbb7959701
Effiba <effiba@aims.edu.gh>
2022-09-30 01:21:19 -0700

    t0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string ff by new line
+ * @s: str to be printed
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	_putchar("%s\n", s);
+}
