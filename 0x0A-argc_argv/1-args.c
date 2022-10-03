f41901acb5b74a99805df811f2b3921593dee537
Effiba <effiba@aims.edu.gh>
2022-10-03 09:42:17 -0700

    t1: forgot unused att for argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc,__attribute((unused)) char *argv[])
 {
-	printf("%d\n", argcc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
