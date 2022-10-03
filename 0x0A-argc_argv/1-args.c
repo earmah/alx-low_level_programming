0c1166ea7e659fe4034f615be1f7ef1b603fd020
Effiba <effiba@aims.edu.gh>
2022-10-03 09:37:11 -0700

    t1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point; prints number of args
+ * @argc: num of args
+ * @argv: args passed
+ * Return: 0
+ */
+
+int main (int c, char *argv[])
+{
+	printf("%d\n", c - 1);
+	return (0)
+}
