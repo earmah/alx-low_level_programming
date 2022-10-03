5cf9057c699d534b30983c10d613ff45f2cb0f1d
Effiba <effiba@aims.edu.gh>
2022-10-03 09:34:22 -0700

    t0; remove main.h; forgot [] on argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -12,6 +11,7 @@
 
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf("%s \n", argv[0]);
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
