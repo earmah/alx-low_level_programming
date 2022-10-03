3a261a8fdf3baacbcf03871f05c67db16a785b7e
Effiba <effiba@aims.edu.gh>
2022-10-03 10:52:30 -0700

    t4: needed isInt function

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,11 +8,27 @@
  * Return: 0
  */
 
+int isInt(char *c)
+{
+	while (*c != '\0')
+	{
+		if (isdigit(*c) == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			c++;
+		}
+	}
+	return (1);
+}
+
 int main(int argc, char *argv[])
 {
 	int count;
 	int sum = 0;
-	
+
 	if (argc <= 1)
 	{
 		printf("0\n");
@@ -20,11 +36,12 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		for (count = 1; count < argc - 1; count++)
+		for (count = 1; count < argc; count++)
 		{
-			if (isdigit(argv[count]))
+			/*Check if all ints are odd or even */
+			if (isInt(argv[count]) == 1)
 			{
-				sum += argv[count];
+				sum += atoi(argv[count]);
 			}
 			else
 			{
