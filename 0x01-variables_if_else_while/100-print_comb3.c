852c323e54fdb39bedc07a042ecbd7cff1c94218
Effiba <effiba@aims.edu.gh>
2022-10-06 20:23:41 -0700

    tx:102

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point_ print num from 0 to 99
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ones, tens;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		putchar(tens);
+		putchar(ones);
+
+		if (tens != '8' || ones != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print all combos of 3 diff digit
+ * Return: 0
+ */
+
+int main(void)
+{
+	int one, ten, hundred;
+
+	for (hundred = '0'; hundred <= '9'; hundred++)
+	{
+		for (ten = (hundred + 1); ten <= '9; ten++)
+		{
+			for (one = (ten + 1); one <= '9'; one++)
+			{
+				putchar(hundred);
+				putchar(ten);
+				putchar(one);
+				
+				if (hundred != '7' || ten != '8' || one != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
