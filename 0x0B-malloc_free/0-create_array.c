88c66b16959b1ee2f9b35b0e857f07bbda16413d
Effiba <effiba@aims.edu.gh>
2022-10-12 08:01:58 -0700

    t0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_array - creates arr of chars and
+ * initializes it with a specific char
+ * @size: size of mem to print
+ * @c: char to convert to arr
+ *
+ * Return: pointer to the arr or NULL if failed
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+	int count;
+
+	str = malloc(sizeof(char) * size)
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (count = 0; count < size; count++)
+		{
+			str[count] = c;
+		}
+	}
+	return (str);
+}
+
+
