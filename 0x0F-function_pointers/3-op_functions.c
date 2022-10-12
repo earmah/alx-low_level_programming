1e871782d0131b85a813cc0e9255348651a334eb
Effiba <effiba@aims.edu.gh>
2022-10-12 16:04:12 -0700

    t3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - adds two ints
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two ints
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two ints
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two ints
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod of two ints
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
