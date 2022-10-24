cb156810dbe001368734b580a5c6524092b5b032
Effiba <effiba@aims.edu.gh>
2022-10-24 06:36:41 -0700

    more pointers t7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,5 +8,25 @@
 
 char *leet(char *s)
 {
-	return ('\0');
+	int count = 0; 
+	int count_alpha;
+	int up[] = {65, 69, 76, 79, 84};
+	int lo[] = {97, 101, 108, 111, 116};
+	int num[] = {52, 51, 49, 48, 55};
+
+	while (s[count] != '\0')
+	{
+		count_alpha = 0;
+		for (count_alpha = 0; count_alpha < 5; count_alpha++)
+		{
+			if (s[count] == up[count_alpha] ||
+				s[count] == lo[count_alpha])
+			{
+				s[count] = num[count_alpha];
+			}
+		}
+		count++;
+	}
+
+	return (s);
 }
