340571b633dbf6e59cbc758ae25e283ca1ac6b97
Effiba <effiba@aims.edu.gh>
2022-10-24 04:55:20 -0700

    more pointers, t6: including other separator conditions

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,7 +20,12 @@ char *cap_string(char *s)
 		{
 			/*printf("Passed 2nd while loop\n");*/
 			if ((s[count - 1] == 46 || s[count - 1] == 32 ||
-				s[count - 1] == '\n' || s[count - 1] == '\t')
+				s[count - 1] == 10 || s[count - 1] == 9 ||
+				s[count - 1] == 44 || s[count - 1] == 59 ||
+				s[count - 1] == 33 || s[count - 1] == 63 ||
+				s[count - 1] == 34 || s[count - 1] == 40 ||
+				s[count - 1] == 41 || s[count - 1] == 123 ||
+				s[count - 1] == 125)
 					&& s[count] > 96 && s[count] < 123)
 			{
 				/*printf("entered for loop\n");*/
