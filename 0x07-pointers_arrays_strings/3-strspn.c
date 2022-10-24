43224ce543004a7d3857af4da474a5386762eec1
Effiba <effiba@aims.edu.gh>
2022-10-24 07:41:27 -0700

    even more ptr, t3: check seg fault

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,27 +7,28 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int count, r, c;
+	/*Vars to count num of char in args, and num of matches */
+	unsigned int count_cmp, len, acc_val, count_str = 0;
 
-	count = 0;
-	while (s[count] != '\0')
+	for (count_cmp = 0; accept[count_cmp] != '\0'; count_cmp++)
 	{
-		r = 0;
-		c = 0;
-		while (accept[r] != '\0')
-		{
-			if (s[count] == accept[r])
-			{
-				c = 0;
-				break;
-			}
-			r++;
-		}
-		if (c == 1)
-		{
-			break;
-		}
-		count++;
+		/*num of char in accept arg */
+		acc_val += 1;
 	}
-	return (count);
+	/*printf("Number of char in accept: %d", acc_val);*/
+
+	while (s[count_str] != 32)
+	{
+		count_cmp = 0;
+		/*Iterate through accept and cmp each char with curr s*/
+		for (count_cmp = 0; count_cmp <= acc_val; count_cmp++)
+		{
+			if (s[count_str] == accept[count_cmp])
+			{
+				len += 1;
+			}
+		}
+		count_str++;
+	}
+	return (len);
 }
