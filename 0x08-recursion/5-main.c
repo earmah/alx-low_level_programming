f8c60a7c573d1efe03ba46a844a2ae182ace1f27
Effiba <effiba@aims.edu.gh>
2022-10-26 13:37:47 -0700

    recursion t5

---
+++ 0x08-recursion/5-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _sqrt_recursion(1);
+	printf("%d\n", r);
+	r = _sqrt_recursion(1024);
+	printf("%d\n", r);
+	r = _sqrt_recursion(-1);
+	printf("%d\n", r);
+	return (0);
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,31 @@
-#include <math.h>
 #include "main.h"
+
+/**
+ * recursion_wrapper - estimates sqrt root and calculates square rt
+ * @lower_bd: lower bound estimation val
+ *
+ * Return: possible sqrt
+ */
+int recursion_wrapper(int lower_bd, int num)
+{
+	if (lower_bd * lower_bd == num)
+	{
+		return (lower_bd);
+	}
+	else if (lower_bd * lower_bd <= num)
+	{
+		/*Possibly next int produces correct num*/
+		lower_bd += 1;
+		return (recursion_wrapper(lower_bd, num));
+	}
+	else
+	{
+		/*In this case l_bd squared is greater than num; no natural root exists*/
+		return (-1);
+	}
+}
+
+
 
 /**
  * _sqrt_recursion - calculate natural sqrt of a number
@@ -10,11 +36,16 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0 || n == NULL)
+	if (n < 0)
 	{
 		return (-1);
 	}
-
-	return (sqrt(n));
-
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (recursion_wrapper(2, n));
+	}
 }
