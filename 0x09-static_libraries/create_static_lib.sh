78e566f0267b9a596cfecb4f14caf728b48ecdd8
Effiba <effiba@aims.edu.gh>
2022-11-01 04:49:48 -0700

    static lib, t1: forgot - before std=gnu89

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -2,6 +2,6 @@
 
 /* Creates a static library from all the files w/i curr directory*/
 
-gcc -Wall -Wextra -Werror -pedantic std=gnu89 -c *.c
+gcc -Wall -Wextra -Werror -pedantic -std=gnu89 -c *.c
 ar -rc liball.a *.o
 ranlib liball.a
