826359a41d96503356ba0516918f710afc3a5490
Effiba <effiba@aims.edu.gh>
2022-11-07 01:34:29 -0800

    malloc, t2: fooled by my own error message

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,16 @@ char *str_concat(char *s1, char *s2)
 	int i, count1 = 0, j = 0, count2 = 0;
 	char *s_conc;
 
+	if (!s1 || !s2)
+	{
+		return (NULL);
+	}
+
 	/* Count num of chars in s1 & s2*/
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		count1++;
-		}
+	}
 
 	for (i = 0; s2[i] != '\0'; i++)
 	{
@@ -27,7 +32,10 @@ char *str_concat(char *s1, char *s2)
 
 	/*Allocate needed mem for s_conc*/
 	s_conc = malloc(sizeof(char) * (count1 + count2));
-
+	if (s_conc == NULL)
+	{
+		return (NULL);
+	}
 	/*Copy each char to s_conc*/
 	for (i = 0; i < count1; i++)
 	{
