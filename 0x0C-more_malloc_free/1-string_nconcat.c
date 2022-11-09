817123910046889de789be5b94b7a51e8e7d4b01
Effiba <effiba@aims.edu.gh>
2022-11-09 12:15:11 -0800

    more mal, t1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,12 +2,77 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - allocated memory of size b using malloc
- * @b: size of mem to allocate
+ * str_ln - size of str
+ * @s: str to count
  *
- * Return: pointer to allocated mem
+ * Return: size of s
  */
 
-void *malloc_checked(unsigned int b)
+int str_ln(char *s)
 {
+	unsigned int i = 0, count;
+
+	if (s == NULL)
+	{
+		return (i);
+	}
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * string_nconcat - concats two strings
+ * @s1: first str
+ * @s2: second str
+ * @n: num of bytes taken from s2
+ *
+ * Return: pointer to newly alloc'd space
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *conc;
+	unsigned int i = 0, count, s1_ln, s2_ln;
+
+	s1_ln = str_ln(s1);
+	s2_ln = str_ln(s2);
+	conc = malloc(sizeof(char) * (s1_ln + n + 1));
+	if (conc == NULL)
+	{
+		return (NULL);
+	}
+	if (s1_ln + s2_ln == 0)
+	{
+		return ("");
+	}
+	else if (s2_ln == 0)
+	{
+		for (count = 0; count <= s1_ln; count++)
+		{
+			conc[count] = s1[count];
+		}
+		return (conc);
+	}
+	else if (s1_ln == 0)
+	{
+		for (count = 0; count < n; count++)
+		{
+			conc[count] = s2[count];
+			return (conc);
+		}
+		for (; s2_ln < n; count++)
+		{
+			conc[count] = '\0';
+		}
+	}
+	for (count = s1_ln, i = 0; count <= s1_ln + n; count++, i++)
+	{
+		conc[count] = s2[i];
+	}
+	conc[count++] = '\0';
+	return (conc);
 }
