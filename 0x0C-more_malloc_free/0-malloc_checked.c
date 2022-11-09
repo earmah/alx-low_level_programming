16a077684d551c8a28f6c0e6f833f931dddaf67a
Effiba <effiba@aims.edu.gh>
2022-11-09 06:51:54 -0800

    more mal, t0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocated memory of size b using malloc
+ * @b: size of mem to allocate
+ *
+ * Return: pointer to allocated mem
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mal;
+
+	mal = malloc(b);
+
+	if (mal == NULL)
+	{
+		_putchar(98);
+		return (NULL);
+	}
+	return (mal);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocated memory of size b using malloc
+ * @b: size of mem to allocate
+ *
+ * Return: pointer to allocated mem
+ */
+
+void *malloc_checked(unsigned int b)
+{
+}
