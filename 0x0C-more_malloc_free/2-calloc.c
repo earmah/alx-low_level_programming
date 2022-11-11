694e0654b7d76366cc4064b693ee0182af647160
Effiba <effiba@aims.edu.gh>
2022-11-11 07:30:35 -0800

    more mal, t2: init arr to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,24 @@
 #include "main.h"
 
 /**
+ * _memset - initialize array to 
+ * @arr: array
+ * @c: constant byte to fill arr
+ * @size: size of array/ num of elems
+ */
+
+unsigned int *_memset(unsigned int *arr, unsigned int c, unsigned int size)
+{
+	unsigned int count;
+
+	for (count = 0; count < size; count++)
+	{
+		arr[count] = c;
+	}
+	return (arr);
+}
+
+/**
  * calloc - allocates memory for an array using malloc
  * @nmemb: num of elems in array
  * @size: bit size of elem in array
@@ -12,6 +30,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *clc;
+	unsigned int init = 0;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -25,5 +44,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
+	_memset(clc, init, nmemb);
 	return (clc);
 }
