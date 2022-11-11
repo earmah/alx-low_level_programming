967a07509b548c2fc469227e069c171267acfe13
Effiba <effiba@aims.edu.gh>
2022-11-11 04:42:12 -0800

    more mal, t1: null entry handle

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@ int str_ln(char *s)
 {
 	unsigned int i = 0, count;
 
-	if (s == NULL)
+	if (s == NULL || s[0] == '\0')
 	{
 		return (i);
 	}
@@ -33,8 +33,8 @@ int str_ln(char *s)
 
 unsigned int min(unsigned int a, unsigned int b)
 {
-	return (a < b) ? a : b;
-}	
+	return ((a < b) ? a : b);
+}
 
 /**
  * string_nconcat - concats two strings
@@ -50,30 +50,34 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *conc;
 	unsigned int i = 0, count, s1_ln, s2_ln;
 
+	s1_ln = str_ln(s1);
+	s2_ln = str_ln(s2);
 	if (str_ln(s1) + str_ln(s2) == 0)
-		return ("");
-	else
-	{
-		s1_ln = str_ln(s1);
-		s2_ln = str_ln(s2);
-	}
+		return ('\0');
 	conc = malloc(sizeof(char) * (s1_ln + min(n, s2_ln) + 1));
 	if (conc == NULL)
 		return (NULL);
-
-	for (count = 0; count < s1_ln; count++)
-	{
-		conc[count] = s1[count];
-	}
-	if (s2_ln == 0)
-		return (conc);
-	else if (s1_ln == 0)
+	if (s1_ln == 0)
 	{
 		for (count = 0; count < min(n, s2_ln); count++)
 		{
 			conc[count] = s2[count];
 		}
+		conc[count] = '\0';
 		return (conc);
+	}
+	else if (s2_ln == 0)
+	{
+		for (count = 0; count < s1_ln; count++)
+		{
+			conc[count] = s1[count];
+		}
+		conc[count] = '\0';
+		return (conc);
+	}
+	for (count = 0; count < s1_ln; count++)
+	{
+		conc[count] = s1[count];
 	}
 	for (count = s1_ln, i = 0; count < s1_ln + min(n, s2_ln); count++, i++)
 	{
