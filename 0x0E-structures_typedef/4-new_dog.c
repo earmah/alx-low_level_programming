c3df484273212a8c1151a586e6e5cfe387b833df
Effiba <effiba@aims.edu.gh>
2022-11-14 15:05:00 -0800

    struct, t4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,57 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * check_mal - check if mal is NULL
+ * @ptr: ptr to malloc
+ *
+ * Return: char * to ptr or NULL
+ */
+
+char *check_mal(char *ptr)
+{
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (ptr);
+	}
+}
+
+/**
+ * new_dog - creates a new dog using typedef
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Return: pointer to dog_t
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_dog;
+
+	n_dog = malloc(sizeof(dog_t));
+
+	if (n_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	n_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	check_mal(n_dog->name);
+	strcpy(n_dog->name, name);
+
+	n_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	check_mal(n_dog->owner);
+	strcpy(n_dog->owner, owner);
+
+	n_dog->age = age;
+
+	return (n_dog);
+}
