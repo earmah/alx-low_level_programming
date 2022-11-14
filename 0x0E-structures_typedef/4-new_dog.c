495b9db4176c17c8e726eb625f2daae3e146b1c2
Effiba <effiba@aims.edu.gh>
2022-11-14 15:11:31 -0800

    struct, t4: case of failed malloc, need to free all other mallocs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -53,5 +53,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	n_dog->age = age;
 
+	if (n_dog == NULL || n_dog->name == NULL || n_dog->owner == NULL)
+	{
+		free(n_dog);
+		free(n_dog->name);
+		free(n_dog->owner);
+	}
 	return (n_dog);
 }
