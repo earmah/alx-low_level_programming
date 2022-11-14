a5de6bba1ca73f3c3548ac62c30a9dfcaa06ccff
Effiba <effiba@aims.edu.gh>
2022-11-14 07:45:59 -0800

    struct, t0: betty style description missing

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct dog - struct define characs of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: create identification params for a dog
+ */
+
 struct dog
 {
 	char *name;
